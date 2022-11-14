c981b91809c123d8c67ff8b746862fb78709318c
haring000 <pillpill1207@gmail.com>
2022-11-14 19:24:26 +0700

    first commit

---
+++ listings/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+from .models import Listing
+
+class ListingFrom(ModelForm):
+    class Meta:
+        model = Listing
+        fields = [
+            "title",
+            "price",
+            "num_bedrooms",
+            "num_bathrooms",
+            "square_footage",
+            "address",
+        ]
---
+++ listings/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from .models import Listing
+from .forms import ListingFrom
+
+# Create your views here.
+# Create CRUD
+
+def listing_list(request):
+    listings = Listing.objects.all()
+    context = {
+        "listings" : listings
+    }
+    return render(request, 'listings.html', context)
+
+def listing_retrieve(request, pk):
+    listing = Listing.objects.get(id=pk)
+    context = {
+        "listing" : listing
+    }
+    return render(request, 'listing.html', context)
+
+def listing_create(request):
+    form = ListingFrom()
+    if request.method == "POST":
+        form = ListingFrom(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    context = {
+        "form": form
+    }
+    return render(request, "listing_create.html", context)
+
+def listing_update(request, pk):
+    listing = Listing.objects.get(id=pk)
+    form = ListingFrom(instance=listing)
+
+
+    if request.method == "POST":
+        form = ListingFrom(request.POST, instance=listing)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    context = {
+        "form": form
+    }
+    return render(request, "listing_update.html", context)    
+
+def listing_delete(request, pk):
+    listing = Listing.objects.get(id=pk)
+    listing.delete()
+    return redirect("/")
